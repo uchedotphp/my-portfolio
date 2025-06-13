7fc5b3bbbaebafd9e750e272a9866a708e94f2d2
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-13 07:16:02 +0100

    Styles: create design color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold bg-red-500 underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold bg-orange-500 underline">Hello world!</h1>;
 }
 
 export default App;
