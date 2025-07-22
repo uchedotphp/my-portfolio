8a3a2d03d84d57673ee844549653fa108b5cebd4
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-07-22 09:25:32 +0100

    refactor: update main entry point for app initialization

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./routing/routes";
-import "./assets/styles/index.css";
+import "@styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
